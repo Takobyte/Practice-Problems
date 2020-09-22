24d360aa24d3f6819da0cc8446f473d198cc3b1a
John So <jungwookso@gmail.com>
2020-09-21 18:32:08 -0700

    finding median of sorted arrays

--- src/MedianSortedArrays/Solution.java
+++ src/MedianSortedArrays/Solution.java
@@ -1,4 +1,67 @@
 package MedianSortedArrays;
 
 public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i1 = 0;
+        int i2 = 0;
+        int num1,num2;
+        int numsL1 = nums1.length;
+        int numsL2 = nums2.length;
+        int resultLength = nums1.length + nums2.length;
+        int[] result = new int[resultLength];
+
+        // return 0 if both arrays are empty
+        if (resultLength == 0) {
+            return 0.0000;
+        }
+
+        // merge two sorted arrays
+        if (numsL1 <= 0 || numsL2 <= 0) {
+            result = numsL1 > numsL2 ? nums1 : nums2;
+        } else {
+            for (int i = 0; i < resultLength; i++) {
+                try {
+                    num1 = nums1[i1];
+                } catch (Exception e) {
+                    try {
+                        result[i] = nums2[i2];
+                        i2++;
+                        continue;
+                    }
+                    catch (Exception e1){
+                        continue;
+                    }
+                }
+                try {
+                    num2 = nums2[i2];
+                } catch (Exception e) {
+                    try {
+                        result[i] = nums1[i1];
+                        i1++;
+                        continue;
+                    } catch(Exception e1) {
+                        continue;
+                    }
+                }
+                if (num1 <= num2) {
+                    result[i] = nums1[i1];
+                    i1++;
+                } else {
+                    result[i] = nums2[i2];
+                    i2++;
+                }
+            }
+        }
+
+        // calculate median
+        if (resultLength % 2 == 0) {
+            int resultIndex = (resultLength / 2) - 1;
+            double median = (result[resultIndex] + result[resultIndex + 1]) / 2.0;
+            return median;
+        } else {
+            int resultIndex = (resultLength / 2);
+            double median = result[resultIndex];
+            return median;
+        }
+    }
 }
--- src/MedianSortedArrays/SolutionTest.java
+++ src/MedianSortedArrays/SolutionTest.java
@@ -1,5 +1,96 @@
+package MedianSortedArrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+    Solution obj;
 
+    @BeforeEach
+    void setUp() {
+        obj = new Solution();
+    }
+
+    @Test
+    void testNumsOneNumsEach() {
+        int[] nums1 = {1};
+        int[] nums2 = {2};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 1.50000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsUnevenArrays() {
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 2.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsEvenArrays() {
+        int[] nums1 = {1,2};
+        int[] nums2 = {3,4};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 2.50000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testZeroNumsEvenArrays() {
+        int[] nums1 = {0,0};
+        int[] nums2 = {0,0};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 0.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testZeroNumsUnevenArrays() {
+        int[] nums1 = {0};
+        int[] nums2 = {0,0};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 0.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsEmptyNums1() {
+        int[] nums1 = {};
+        int[] nums2 = {1};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 1.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsEmptyNums2() {
+        int[] nums1 = {2};
+        int[] nums2 = {};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 2.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsBothEmpty() {
+        int[] nums1 = {};
+        int[] nums2 = {};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 0.00000;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNumsComplex() {
+        int[] nums1 = {1};
+        int[] nums2 = {2,3,4};
+        double result = obj.findMedianSortedArrays(nums1,nums2);
+        double expected = 2.50000;
+        assertEquals(expected, result);
+    }
 }
